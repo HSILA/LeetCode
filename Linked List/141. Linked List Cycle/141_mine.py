b4d51dc05624b14fc2fdc456d09ac9bcc1ea93cf
Ali Shiraee <a.shiraee@gmail.com>
2024-10-15 23:13:47 -0400

    Add 141. Linked List Cycle

---
+++ Linked List/141. Linked List Cycle/141_mine.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/linked-list-cycle/
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while head and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+
+        return False
